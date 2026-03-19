ded714da45f4ca1d19c00bb3e1581aea75864a99
Iggie Wang <iggiewang@gmail.com>
2026-03-19 16:30:35 +0800

    Move layer-level batch decompression parallelism into C++ extension

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
-from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension, CppExtension
 
 setup(
     name="kvcache_compression",
     version="0.1.0",
     packages=find_packages(include=["quant", "quant.*", "huffman", "huffman.*"]),
     ext_modules=[
+        CppExtension(
+            name="huffman._C",
+            sources=["huffman/huffman_ext.cpp"],
+            extra_compile_args={
+                "cxx": ["-O3"],
+            },
+        ),
         CUDAExtension(
             name="quant.int4_ext._C",
             sources=["quant/int4_ext/int4_ext.cpp", "quant/int4_ext/int4_ext_cuda.cu"],
